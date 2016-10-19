b9e8a6ac7a4ab313b1437cfafd175cd79d1ca02b
ducky <richard.lin@berkeley.edu>
2016-10-18 17:36:43 -0700

    Make the test functional. Yayyy!

--- src/test/scala/chiselTests/NamingAnnotationTest.scala
+++ src/test/scala/chiselTests/NamingAnnotationTest.scala
@@ -7,42 +7,56 @@ import org.scalatest._
 import org.scalatest.prop._
 import chisel3.testers.BasicTester
 
-object FunctionMockup2 {
+import scala.collection.mutable.ListBuffer
+
+@chiselName
+class NamedModule extends BasicTester {
+  val expectedNameMap = ListBuffer[(Data, String)]()
+
   @chiselName
-  def apply(): UInt = {
+  def FunctionMockup2(): UInt = {
     val my2A = UInt(1)
     val my2B = my2A +& UInt(2)
     val my2C = my2B +& UInt(3)
+
+    expectedNameMap += ((my2B, "test_myNested_my2B"))
+
     my2C
   }
-}
 
-object FunctionMockup {
   @chiselName
-  def apply(): UInt = {
+  def FunctionMockup(): UInt = {
     val myNested = FunctionMockup2()
     val myA = UInt(1) + myNested
     val myB = myA +& UInt(2)
     val myC = myB +& UInt(3)
 
+    expectedNameMap += ((myNested, "test_myNested"))
+    expectedNameMap += ((myA, "test_myA"))
+    expectedNameMap += ((myB, "test_myB"))
+
     myC +& UInt(4)
   }
-}
 
-@chiselName
-class NamedModule extends BasicTester {
   val test = FunctionMockup()
   val test2 = test +& UInt(2)
+
+  expectedNameMap += ((test, "test"))
+  expectedNameMap += ((test2, "test2"))
 
   stop()
 }
 
-/** A simple test that checks the recursive function val naming annotation.
-  * Doesn't actually check the FIRRTL result, only that the macro is able to compile and run.
-  * Verification (that naming actually happens) is done separately and manually.
+/** A simple test that checks the recursive function val naming annotation both compiles and
+  * generates the expected names.
   */
 class NamingAnnotationSpec extends ChiselPropSpec {
   property("NamedModule should elaborate") {
-    assertTesterPasses { new NamedModule }
+    var module: NamedModule = null
+    assertTesterPasses { module = new NamedModule; module }
+
+    for ((ref, name) <- module.expectedNameMap) {
+      assert(ref.instanceName == name)
+    }
   }
 }
